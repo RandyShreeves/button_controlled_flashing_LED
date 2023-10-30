a43df96922a5be5b49e9242fa4302f8949c18514
Randy Shreeves <114593302+RandyShreeves@users.noreply.github.com>
2023-10-30 15:32:40 -0400

    Modify delay function to utilize System Timer
    I modified the code in the delay function to create a 100ms delay
    utilizing the System Timer (SysTick). I also changed some variable and
    subroutine names so they were all consistent with snake case naming
    convention. I updated the README and added a picture of the physical
    circuit as well.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 This system uses a programmed TM4C123G microcontroller to allow a user
 to push a button that causes an LED to blink.
 The LED is in the on state when the switch it not pressed.
-The LED blinks at ~5Hz or 100ms intervals while button is pressed.
+The LED blinks at 5Hz or 100ms intervals while button is pressed.
--- push_to_blink.c
+++ push_to_blink.c
@@ -3,7 +3,7 @@
 // The LED is in the on state when the switch it not pressed.
 // The LED blinks at ~5Hz or 100ms intervals while button is pressed.
 // Author: Randy Shreeves
-// Date: 10/25/2023
+// Date: 10/30/2023
 
 // 1. Pre-processor Directives Section
 #define SYSCTL_RCGC2_R (*((volatile unsigned long *)0x400FE108))
@@ -13,22 +13,26 @@
 #define GPIO_PORTE_DIR_R (*((volatile unsigned long *)0x40024400))
 #define GPIO_PORTE_AFSEL_R (*((volatile unsigned long *)0x40024420))
 #define GPIO_PORTE_DEN_R (*((volatile unsigned long *)0x4002451C))
+#define NVIC_ST_CTRL_R (*((volatile unsigned long *)0xE000E010))
+#define NVIC_ST_RELOAD_R (*((volatile unsigned long *)0xE000E014))
+#define NVIC_ST_CURRENT_R (*((volatile unsigned long *)0xE000E018))
 
 // 2. Global Declarations Section
-void delay_1ms(unsigned long msec);
-void portE_initialization(void);
+void port_e_initialization(void);
+void sys_tick_initialization(void);
+void delay_100ms(void);
 
 // 3. Subroutines Section
 int main(void){ 
-	// Initialize Port
-	portE_initialization();
 	// Setup
 	unsigned long input_switch;
+	port_e_initialization();
+	sys_tick_initialization();
 	GPIO_PORTE_DATA_R |= 0x02; // LED starts in the on state
 	// Loop
   while(1){
 		input_switch = GPIO_PORTE_DATA_R&0x01;
-		delay_1ms(100);
+		delay_100ms();
 		if (input_switch == 0x01){		// if switch is pressed
 			GPIO_PORTE_DATA_R ^= 0x02;	// toggle LED
 		}
@@ -38,15 +42,7 @@ int main(void){
   }// end while loop
 }// end main
 
-void delay_1ms(unsigned long msec){
-	unsigned long iterations = (msec * 4000);
-	unsigned long i;
-	for (i = 0; i < iterations; i++){
-		__asm__ volatile("nop");
-	}
-}// end delay_1ms()
-
-void portE_initialization(void){
+void port_e_initialization(void){
 	unsigned long volatile delay; // delay for 3-5 bus cycles for clock to stabilize
 	SYSCTL_RCGC2_R |= 0x10;
 	delay = SYSCTL_RCGC2_R;
@@ -57,3 +53,16 @@ void portE_initialization(void){
 	GPIO_PORTE_AFSEL_R &= ~0x03;
 	GPIO_PORTE_DEN_R |= 0x03;
 } // end portE_initialization
+
+void sys_tick_initialization(void){
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R = 1599999;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_CTRL_R = 0x00000005;
+}//end SysTick_initialization
+
+void delay_100ms(void){
+	while((NVIC_ST_CTRL_R&0x00010000) == 0) {
+		// wait for count flag to be set
+  }
+}// end delay_1ms()
